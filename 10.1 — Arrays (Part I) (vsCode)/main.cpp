42d565da7c06c7e6271a798ffc6230ab8e24f84a
Mateusz Kacperski <oureyelet@gmail.com>
2021-08-04 19:11:22 +0100

    -

--- 10.1 — Arrays (Part I) (vsCode)/main.cpp
+++ 10.1 — Arrays (Part I) (vsCode)/main.cpp
@@ -7,9 +7,29 @@ int main()
     std::cout << std::endl;
     /////////////////////////////////////////////////////////////////////////////
     std::cout << "/////////////////////////////////////////////////////" << '\n';
-    std::cout << "enum" << '\n';
+    std::cout << "Arrays (Part I)" << '\n';
     std::cout << "/////////////////////////////////////////////////////" << '\n';
     /////////////////////////////////////////////////////////////////////////////
+    /*
+    Consider the case where you want to record the test scores for 30 students in a class:
+    */
+    // allocate 30 integer variables (each with a different name)
+    int testScoreStudent1{};
+    int testScoreStudent2{};
+    int testScoreStudent3{};
+
+    //...
+
+    int testScoreStudent30{};
+
+    /*
+    Arrays give us a much easier way to do this. 
+    The following array definition is essentially equivalent:
+    */
+    int testScoreStudent[30]{};// allocate 30 integer variables in a fixed array.
+    
+     
+    
 
     return 0;
 }
