989bb7304cdc3b9b1ce0f8a0f8d7aec0b3339943
Mateusz Kacperski <oureyelet@gmail.com>
2021-08-10 19:39:29 +0100

    quiz time = i live it :) Arrays, Loops BUM !

--- 10.3 — Arrays and loops (vsCode)/main.cpp
+++ 10.3 — Arrays and loops (vsCode)/main.cpp
@@ -83,5 +83,126 @@ int main()
     std::cout << "The best score was " << search << '\n';
 
 
+    std::cout << std::endl;
+    /////////////////////////////////////////////////////////////////////////////
+    std::cout << "/////////////////////////////////////////////////////" << '\n';
+    std::cout << "Mixing loops and arrays" << '\n';
+    std::cout << "/////////////////////////////////////////////////////" << '\n';
+    /////////////////////////////////////////////////////////////////////////////
+
+
+    std::cout << std::endl;
+    /////////////////////////////////////////////////////////////////////////////
+    std::cout << "/////////////////////////////////////////////////////" << '\n';
+    std::cout << "Arrays and off-by-one errors" << '\n';
+    std::cout << "/////////////////////////////////////////////////////" << '\n';
+    /////////////////////////////////////////////////////////////////////////////
+    /*
+    When using loops with arrays, always double-check your loop conditions to make 
+    sure you do not introduce off-by-one errors.
+    */
+
+
+    std::cout << std::endl;
+    /////////////////////////////////////////////////////////////////////////////
+    std::cout << "/////////////////////////////////////////////////////" << '\n';
+    std::cout << "Quiz TIME ! :D" << '\n';
+    std::cout << "/////////////////////////////////////////////////////" << '\n';
+    /////////////////////////////////////////////////////////////////////////////
+    /*
+    Question #1
+    Print the following array to the screen using a loop:
+	
+    constexpr int array[]{ 4, 6, 7, 3, 8, 2, 1, 9, 5 };
+
+    Hint: You can use std::size (as of C++17) or the sizeof() trick (prior to C++17) 
+    to determine the array length.
+    */
+    constexpr int array_Quiz[]{ 4, 6, 7, 3, 8, 2, 1, 9, 5 };
+    constexpr int numArray_Quiz{ static_cast<int>(std::size(array_Quiz)) };
+
+    for(int count{ 0 }; count < numArray_Quiz; ++count)
+    {
+        std::cout << array_Quiz[count] << ' ';
+    }
+    std::cout << std::endl;
+
+
+    /*
+    Question #2
+    Given the array in question 1:
+
+    Ask the user for a number between 1 and 9. If the user does not enter a number between 1 and 9, 
+    repeatedly ask for an integer value until they do. Once they have entered a number between 1 and 9, 
+    print the array. Then search the array for the value that the user entered and print 
+    the index of that element.
+
+    You can test std::cin for invalid input by using the following code:
+	
+    // if the user entered something invalid
+    if (std::cin.fail())
+    {
+        std::cin.clear(); // reset any error flags
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
+        // ignore any characters in the input buffer
+    }
+    */
+    int user_choice{};
+    do
+    {
+        std::cout << "Enter a number between 1 and 9: ";
+        std::cin >> user_choice;
+        
+        // if the user entered an invalid character
+        if (std::cin.fail())
+        {
+        std::cin.clear(); // reset any error flags
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); 
+        // ignore any characters in the input buffer
+        }
+        
+    } while (user_choice < 1 || user_choice > 9);
+    
+    for(int count{ 0 }; count < numArray_Quiz; ++count)
+    {
+        std::cout << array_Quiz[count] << ' ';
+    }
+    std::cout << std::endl;
+
+    for(int count{ 0 }; count < numArray_Quiz; ++count)
+    {
+        if(user_choice == array_Quiz[count])
+        {
+            std::cout << count << '\n';
+
+            break;// since each # in the array is unique, no need to search rest of array
+        }
+    }
+    std::cout << std::endl;
+    
+    
+    /*
+    Question #3
+
+    Modify the following program so that instead of having maxScore hold the largest score directly, 
+    a variable named maxIndex holds the index of the largest score. 
+    */
+    constexpr int scores_Quiz[]{ 84, 92, 76, 81, 56 };
+ 
+    int maxScore_Quiz{ 0 }; // Assume our largest score is 0 to start
+    int maxIndex_Quiz{ 0 };
+ 
+    // now look for a larger score in the full array
+    for (int student_Quiz{ 0 }; student_Quiz < static_cast<int>(std::size(scores_Quiz)); ++student_Quiz)
+    {
+        if (scores_Quiz[student_Quiz] > maxScore_Quiz)
+        {
+            maxScore_Quiz = scores_Quiz[student_Quiz];
+        }
+    }
+ 
+    std::cout << "The best score was " << maxScore_Quiz << '\n';
+
+
     return 0;
 }
