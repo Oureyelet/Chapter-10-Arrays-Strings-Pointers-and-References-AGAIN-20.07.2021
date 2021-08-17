fbb887d24409ea20b0a0cd61c8a532d98deed1ca
Mateusz Kacperski <oureyelet@gmail.com>
2021-08-17 20:10:39 +0100

    -

---
+++ 10.5 — Multidimensional Arrays (vsCode)/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World\n";
+
+    std::cout << std::endl;
+    /////////////////////////////////////////////////////////////////////////////
+    std::cout << "/////////////////////////////////////////////////////" << '\n';
+    std::cout << "Multidimensional array" << '\n';
+    std::cout << "/////////////////////////////////////////////////////" << '\n';
+    /////////////////////////////////////////////////////////////////////////////
+    /*
+    The elements of an array can be of any data type, including arrays! An array of arrays 
+    is called a multidimensional array.
+    */
+    
+
+    return 0;
+}
