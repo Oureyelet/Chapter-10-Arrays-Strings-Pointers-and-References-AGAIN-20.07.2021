b43c933270e3ed0935b4023683856c5500c64de0
MateuszKacperski <oureyelet@gmail.com>
2021-08-25 20:53:55 +0100

    Pointers

--- 10.8 — Introduction to pointers (vsCode)/main.cpp
+++ 10.8 — Introduction to pointers (vsCode)/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <typeinfo> // for: typeid(x).name()
 
 int main()
 {
@@ -109,8 +110,183 @@ int main()
     std::cout << "/////////////////////////////////////////////////////" << '\n';
     /////////////////////////////////////////////////////////////////////////////
     /*
+    Since pointers only hold addresses, when we assign a value to a pointer, that value has to be an address. 
+    One of the most common things to do with pointers is have them hold the address of a different variable.
+
+    To get the address of a variable, we use the address-of operator:
+    */
+    int v{ 5 };
+    int* ptr_V{ &v };// initialize ptr_V with address of variable v
+
+    std::cout << &v << '\n';// print the address of variable v
+    std::cout << ptr_V << '\n'; // print the address that ptr is holding
+
+    //The type of the pointer has to match the type of the variable being pointed to: ! ! ! ! !
+    int iValue{ 7 };
+    double dValue{ 77 };
+
+    int* ptr_iValue{ &iValue };// ok
+    double* ptr_dValue{ &dValue }; // also ok
+
+    std::cout << "dobuel adddress: " << ptr_iValue << '\n';
+/*
+    ptr_iValue = &dValue; // wrong -- int pointer cannot point to the address of a double variable
+    ptr_dValue = &iValue; // wrong -- double pointer cannot point to the address of an int variable
+*/
+    //Note that the following is also not legal:
+    //int* intiger{ 5 };
+    /*
+    This is because pointers can only hold addresses, and the integer literal 5 does not have a memory address. 
+    If you try this, the compiler will tell you it cannot convert an integer to an integer pointer.
     */
 
+
+    std::cout << std::endl;
+    /////////////////////////////////////////////////////////////////////////////
+    std::cout << "/////////////////////////////////////////////////////" << '\n';
+    std::cout << "The address-of operator returns a pointer" << '\n';
+    std::cout << "/////////////////////////////////////////////////////" << '\n';
+    /////////////////////////////////////////////////////////////////////////////   
+    /*
+    It’s worth noting that the address-of operator (&) doesn’t return the address of its operand as a literal. 
+    Instead, it returns a pointer containing the address of the operand, whose type is derived from the argument 
+    (e.g. taking the address of an int will return the address in an int pointer).
+
+    We can see this in the following example:
+    */
+    int x_return{ 7 };
+
+    std::cout << typeid(&x_return).name() << '\n';
+
+
+    std::cout << std::endl;
+    /////////////////////////////////////////////////////////////////////////////
+    std::cout << "/////////////////////////////////////////////////////" << '\n';
+    std::cout << "Indirection through pointers" << '\n';
+    std::cout << "/////////////////////////////////////////////////////" << '\n';
+    /////////////////////////////////////////////////////////////////////////////
+    /*
+    Once we have a pointer variable pointing at something, the other common thing to do with it is indirection 
+    through the pointer to get the value of what it’s pointing at. Indirection through a pointer evaluates to the 
+    contents of the address it is pointing to.
+    */
+    double y_double{ 33 };
+    std::cout << y_double << '\n';// prints contents of value
+    std::cout << &y_double << '\n';// prints address of value
+
+    double* y_double_ptr{ &y_double };// ptr points to value
+    std::cout << y_double_ptr << '\n';// prints address held in ptr, which is &y_double
+    std::cout << *y_double_ptr << '\n';// Indirection through ptr (get the value that ptr is 
+
+
+    /*
+    This is why pointers must have a type. Without a type, when indirecting through a pointer, the pointer wouldn’t know 
+    how to interpret the contents it was pointing to. It’s also why the type of the pointer and the variable address it’s 
+    being assigned to must match. If they did not, indirection through the pointer would misinterpret the 
+    bits as a different type.
+
+    Once assigned, a pointer value can be reassigned to another value:
+    */
+    int value_1{ 333 };
+    int value_2{ 777 };
+
+    int* ptr_value{};
+
+    ptr_value = &value_1;// ptr_value points to value_1
+    std::cout << *ptr_value << std::endl; // prints 333
+
+    ptr_value = &value_2;// ptr_value points to value_2
+    std::cout << *ptr_value; // prints 777
+    /*
+    When the address of variable value is assigned to ptr, the following are true:
+
+    - ptr is the same as &value
+    - *ptr is treated the same as value
+    */
+    std::cout << std::endl;
+    /*
+    Because *ptr is treated the same as value, you can assign values to it just as if it were variable value! 
+    The following program prints 7:
+    */
+    int value_3{ 100 };
+    int* ptr_value_3{ &value_3 };
+
+    std::cout << ptr_value_3 << '\n'; // print address
+    std::cout << *ptr_value_3 << '\n'; // print value
+    std::cout << &value_3 << '\n';// print again address
+
+    *ptr_value_3 = 7; // *ptr_value_3 is the same as value, which is assigned 7
+    std::cout << value_3 << '\n'; // print 7
+
+
+    std::cout << std::endl;
+    /////////////////////////////////////////////////////////////////////////////
+    std::cout << "/////////////////////////////////////////////////////" << '\n';
+    std::cout << "A warning about indirection through invalid pointers" << '\n';
+    std::cout << "/////////////////////////////////////////////////////" << '\n';
+    /////////////////////////////////////////////////////////////////////////////    
+    /*
+    https://www.learncpp.com/cpp-tutorial/introduction-to-pointers/
+    */
+
+
+    std::cout << std::endl;
+    /////////////////////////////////////////////////////////////////////////////
+    std::cout << "/////////////////////////////////////////////////////" << '\n';
+    std::cout << "The size of pointers" << '\n';
+    std::cout << "/////////////////////////////////////////////////////" << '\n';
+    /////////////////////////////////////////////////////////////////////////////
+    /*
+    The size of a pointer is dependent upon the architecture the executable is compiled for -- a 32-bit executable uses 
+    32-bit memory addresses -- consequently, a pointer on a 32-bit machine is 32 bits (4 bytes). With a 64-bit executable, 
+    a pointer would be 64 bits (8 bytes). Note that this is true regardless of the size of the object being pointed to:
+    */
+    char* ptr_char{};// chars are 1 byte
+    int* ptr_int{};// ints are usually 4 bytes
+
+    struct Something
+    {
+        int x{};
+        int y{};
+        int z{};
+    };
+
+    Something* somethingPtr{};// Something is probably 24 bytes
+
+    std::cout << sizeof(ptr_char) << '\n';// print 8
+    std::cout << sizeof(ptr_int) << '\n';// print 8
+    std::cout << sizeof(somethingPtr) << '\n';// print 8
+    
+
+    std::cout << std::endl;
+    /////////////////////////////////////////////////////////////////////////////
+    std::cout << "/////////////////////////////////////////////////////" << '\n';
+    std::cout << "What good are pointers?" << '\n';
+    std::cout << "/////////////////////////////////////////////////////" << '\n';
+    /////////////////////////////////////////////////////////////////////////////
+    /*
+    At this point, pointers may seem a little silly, academic, or obtuse. Why use a pointer if we can just use the original 
+    variable?
+
+    It turns out that pointers are useful in many different cases:
+
+    1) Arrays are implemented using pointers. Pointers can be used to iterate through an array 
+    (as an alternative to array indices) (covered in lesson 10.24 -- Introduction to iterators).
+    2) They are the only way you can dynamically allocate memory in C++ (covered in lesson 10.13 -- Dynamic memory allocation 
+    with new and delete). This is by far the most common use case for pointers.
+    3) They can be used to pass a function as a parameter to another function (covered in lesson 11.7 -- Function Pointers).
+    4) They can be used to achieve polymorphism when dealing with inheritance (covered in lesson 18.1 -- Pointers and references 
+    to the base class of derived objects).
+    5) They can be used to have one struct/class point at another struct/class, to form a chain. This is useful in some more 
+    advanced data structures, such as linked lists and trees.
+
+    So there is actually a surprising number of uses for pointers. But don’t worry if you don’t understand what most of these 
+    are yet. Now that you understand what pointers are at a basic level, we can start taking an in-depth look at the various 
+    cases in which they’re useful, which we’ll do in subsequent lessons.
+    */
+
+
+    
 
 
     return 0;
