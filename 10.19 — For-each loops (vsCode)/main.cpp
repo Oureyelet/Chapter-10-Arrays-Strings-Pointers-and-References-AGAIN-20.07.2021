b159dadd237b7ae4a9507a20fec5ea79254fb73f
MateuszKacperski <oureyelet@gmail.com>
2021-09-14 18:02:32 +0100

    for-each loops (range-based for-loop)

--- 10.19 — For-each loops (vsCode)/main.cpp
+++ 10.19 — For-each loops (vsCode)/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <iterator> // for std::size()
+#include <string> // std::string
+
 
 int main()
 {
@@ -17,7 +19,7 @@ int main()
     constexpr int scores[]{  84, 92, 76, 81, 56  };
     constexpr int numStudents{ std::size(scores) };
 
-    int maxScores{ 0 };
+    int maxScores{ 0 };// keep track of our largest score
     for(int index{ 0 }; index != numStudents; ++index)
     {
         if(scores[index] > maxScores)
@@ -42,8 +44,99 @@ int main()
     std::cout << "//////////////////////////////////////////////////////////////////" << '\n';
     //////////////////////////////////////////////////////////////////////////////////////////
     /*
-    
+    The for-each statement has a syntax that looks like this:
+
+    for (element_declaration : array)
+    statement;
+
+    When this statement is encountered, the loop will iterate through each element in array, assigning the 
+    value of the current array element to the variable declared in element_declaration. For best results, 
+    element_declaration should have the same type as the array elements, otherwise type conversion will occur.
+
+    Let’s take a look at a simple example that uses a for-each loop to print all of the elements in 
+    an array named fibonacci:
     */
+    constexpr int fibonacci[]{ 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 };
+    for( int number : fibonacci )// iterate over array fibonacci    
+    {
+        std::cout << number << ' ';// we access the array element for this iteration through variable number
+    }
+    std::cout << std::endl;
+    /*
+    Let’s take a closer look at how this works. First, the for loop executes, and variable number is set to the value of 
+    the first element, which has value 0. The program executes the statement, which prints 0. Then the for loop executes 
+    again, and number is set to the value of the second element, which has value 1. The statement executes again, 
+    which prints 1. The for loop continues to iterate through each of the numbers in turn, executing the statement for each 
+    one, until there are no elements left in the array to iterate over. At that point, the loop terminates, and the program 
+    continues execution (returning 0 to the operating system).
+
+    Note that variable number is not an array index. It’s assigned the value of the array element for the 
+    current loop iteration.
+
+    Because element_declaration should have the same type as the array elements, this is an ideal case in which to use the 
+    auto keyword, and let C++ deduce the type of the array elements for us.
+    */
+
+
+    std::cout << std::endl;
+    //////////////////////////////////////////////////////////////////////////////////////////
+    std::cout << "//////////////////////////////////////////////////////////////////" << '\n';
+    std::cout << "For-each loops and references" << '\n';
+    std::cout << "//////////////////////////////////////////////////////////////////" << '\n';
+    //////////////////////////////////////////////////////////////////////////////////////////
+    /*
+    In the following for-each example, our element declarations are declared by value:
+    */
+    std::string array[]{ "peter", "likes", "frozen", "yogurt" };
+    for(auto element : array)// element will be a copy of the current array element
+    {
+        std::cout << element << ' ';
+    }
+
+    std::cout << '\n';
+    /*
+    This means each array element iterated over will be copied into variable element. 
+    Copying array elements can be expensive, and most of the time we really just want to refer to the original element. 
+    Fortunately, we can use references for this:
+    */
+    std::string names[]{ "Ania", "Kamila", "Robert", "Holly", "Dacjan", "Mateusz" };
+    // The ampersand makes element a reference to the actual array element, preventing a copy from being made
+    for(auto& element : names)
+    {
+        std::cout << element << ' ';
+    }
+    std::cout << '\n';
+    /*
+    In the above example, element will be a reference to the currently iterated array element, avoiding having to make a copy. 
+    Also any changes to element will affect the array being iterated over, something not possible if 
+    element is a normal variable.
+
+    And, of course, it’s a good idea to make your reference const if you’re intending to use it in a 
+    read-only fashion:
+    */
+    std::string bejbe[]{ "Ania", "Bejbe", "Ponczkowa" };
+    for(const auto& element : bejbe)// element is a const reference to the currently iterated array element
+    {
+        std::cout << element << '\n';
+    }
+    /*
+    Best practice:
+
+    In for-each loops element declarations, if your elements are non-fundamental types, use references or const 
+    references for performance reasons. 
+    */
+
+
+    std::cout << std::endl;
+    //////////////////////////////////////////////////////////////////////////////////////////
+    std::cout << "//////////////////////////////////////////////////////////////////" << '\n';
+    std::cout << "Rewriting the max scores example using a for-each loop" << '\n';
+    std::cout << "//////////////////////////////////////////////////////////////////" << '\n';
+    //////////////////////////////////////////////////////////////////////////////////////////
+    /*
+    Here’s the example at the top of the lesson rewritten using a for each loop:
+    */
+    const int our_scores[]{  }
 
 
     return 0;
